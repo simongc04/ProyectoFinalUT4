ea122b40e7403efe2088b5927506c6c6f8f73b86
simon <simonescuela9@gmail.com>
2024-12-13 10:27:00 +0000

    Archivo Terminado

--- app/src/main/java/com/simon/proyectofinalut4/data/Receta.kt
+++ app/src/main/java/com/simon/proyectofinalut4/data/Receta.kt
@@ -1,3 +1,13 @@
 package com.simon.proyectofinalut4.data
 
-data class Receta()
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "recetas")
+data class Receta(
+    @PrimaryKey(autoGenerate = true) val id: Int = 0,
+    val nombre: String,
+    val descripcion: String,
+    val tiempoPreparacion: String,
+    val categoriaId: Int
+)
