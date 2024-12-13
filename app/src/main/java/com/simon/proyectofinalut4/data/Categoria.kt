7ca540bafdd7f896f63bbe512e17625fe300a6f2
simon <simonescuela9@gmail.com>
2024-12-13 10:28:26 +0000

    Archivo Terminado

--- app/src/main/java/com/simon/proyectofinalut4/data/Categoria.kt
+++ app/src/main/java/com/simon/proyectofinalut4/data/Categoria.kt
@@ -1,3 +1,10 @@
 package com.simon.proyectofinalut4.data
 
-data class Categoria()
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "categorias")
+data class Categoria(
+    @PrimaryKey(autoGenerate = true) val id: Int = 0,
+    val nombre: String
+)
