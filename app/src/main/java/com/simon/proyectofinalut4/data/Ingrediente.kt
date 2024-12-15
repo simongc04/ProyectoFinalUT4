70e3fc601e1d044f11a11a2567ff1c730df2950e
simongc04 <simonescuela9@gmail.com>
2024-12-15 23:23:21 +0000

    terminado

--- app/src/main/java/com/simon/proyectofinalut4/data/Ingrediente.kt
+++ app/src/main/java/com/simon/proyectofinalut4/data/Ingrediente.kt
@@ -1,2 +1,22 @@
 package com.simon.proyectofinalut4.data
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.ForeignKey
+import androidx.room.PrimaryKey
+
+@Entity(
+    tableName = "ingredientes",
+    foreignKeys = [ForeignKey(
+        entity = Receta::class,
+        parentColumns = ["id"],
+        childColumns = ["recetaId"],
+        onDelete = ForeignKey.CASCADE
+    )]
+)
+data class Ingrediente(
+    @PrimaryKey(autoGenerate = true) val id: Long = 0,
+    val nombre: String,
+    val cantidad: String,
+    @ColumnInfo(index = true) val recetaId: Long
+)
