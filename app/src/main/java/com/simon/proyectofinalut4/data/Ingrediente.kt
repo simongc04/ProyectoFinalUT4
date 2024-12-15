ac4a6b02a8de6a25ad670592d975ef631bf7495d
simongc04 <simonescuela9@gmail.com>
2024-12-15 23:22:39 +0000

    realizado

--- app/src/main/java/com/simon/proyectofinalut4/data/Ingrediente.kt
+++ app/src/main/java/com/simon/proyectofinalut4/data/Ingrediente.kt
@@ -1,12 +1,2 @@
 package com.simon.proyectofinalut4.data
 
-import androidx.room.Entity
-import androidx.room.PrimaryKey
-
-@Entity(tableName = "ingredientes")
-data class Ingrediente(
-    @PrimaryKey(autoGenerate = true) val id: Int = 0,
-    val nombre: String,
-    val cantidad: String,
-    val recetaId: Int
-)
