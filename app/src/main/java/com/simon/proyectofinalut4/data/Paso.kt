24606a8661be09278bba28aafc9499f62de84b2b
simongc04 <simonescuela9@gmail.com>
2024-12-15 23:24:12 +0000

    Codigo finalizado

--- app/src/main/java/com/simon/proyectofinalut4/data/Paso.kt
+++ app/src/main/java/com/simon/proyectofinalut4/data/Paso.kt
@@ -1,2 +1,21 @@
 package com.simon.proyectofinalut4.data
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.ForeignKey
+import androidx.room.PrimaryKey
+
+@Entity(
+    tableName = "pasos",
+    foreignKeys = [ForeignKey(
+        entity = Receta::class,
+        parentColumns = ["id"],
+        childColumns = ["recetaId"],
+        onDelete = ForeignKey.CASCADE
+    )]
+)
+data class Paso(
+    @PrimaryKey(autoGenerate = true) val id: Long = 0,
+    val descripcion: String,
+    @ColumnInfo(index = true) val recetaId: Long
+)
