8eecfb61a2cf872a625bdfffca34341cc81b3ede
simongc04 <simonescuela9@gmail.com>
2024-12-15 23:31:42 +0000

    Archivo Terminado

--- app/src/main/java/com/simon/proyectofinalut4/data/IngredienteDao.kt
+++ app/src/main/java/com/simon/proyectofinalut4/data/IngredienteDao.kt
@@ -1,2 +1,20 @@
 package com.simon.proyectofinalut4.data
 
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+
+
+@Dao
+interface IngredienteDao {
+
+    @Insert
+    suspend fun insert(ingrediente: Ingrediente)
+
+    @Delete
+    suspend fun delete(ingrediente: Ingrediente)
+
+    @Query("SELECT * FROM ingredientes WHERE recetaId = :recetaId")
+    suspend fun getByRecetaId(recetaId: Long): List<Ingrediente>
+}
