24da54f0d99305b6b84f8d8347857a1f6ec711f4
simongc04 <simonescuela9@gmail.com>
2024-12-15 23:27:32 +0000

    codigos eleminados

---
+++ app/src/main/java/com/simon/proyectofinalut4/data/AppDatabase .kt
@@ -0,0 +1,28 @@
+package com.simon.proyectofinalut4.data
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [Receta::class], version = 1)
+abstract class AppDatabase : RoomDatabase() {
+    abstract fun recetaDao(): RecetaDao
+
+    companion object {
+        private var INSTANCE: AppDatabase? = null
+
+        fun getDatabase(context: Context): AppDatabase {
+            if (INSTANCE == null) {
+                synchronized(AppDatabase::class.java) {
+                    INSTANCE = Room.databaseBuilder(
+                        context.applicationContext,
+                        AppDatabase::class.java,
+                        "receta_database"
+                    ).build()
+                }
+            }
+            return INSTANCE!!
+        }
+    }
+}
