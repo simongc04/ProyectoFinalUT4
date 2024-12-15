abc29e98bd23f59ffd70ec2e82b4754ac8465ca4
simongc04 <simonescuela9@gmail.com>
2024-12-15 23:36:01 +0000

    Archivo Finalizado view

--- app/src/main/java/com/simon/proyectofinalut4/view/RecetaViewModelFactory.kt
+++ app/src/main/java/com/simon/proyectofinalut4/view/RecetaViewModelFactory.kt
@@ -1,2 +1,18 @@
 package com.simon.proyectofinalut4.view
 
+import android.app.Application
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+
+
+
+class RecetaViewModelFactory(private val application: Application) : ViewModelProvider.Factory {
+
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(RecetaViewModel::class.java)) {
+            @Suppress("UNCHECKED_CAST")
+            return RecetaViewModel(application) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
+}
