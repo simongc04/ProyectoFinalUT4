7a85bb74c47b0c031d43ff744549969c0d389e71
simongc04 <simonescuela9@gmail.com>
2024-12-15 23:31:07 +0000

    Archivo Terminado

--- app/src/main/java/com/simon/proyectofinalut4/data/PasoDao.kt
+++ app/src/main/java/com/simon/proyectofinalut4/data/PasoDao.kt
@@ -1,2 +1,19 @@
 package com.simon.proyectofinalut4.data
 
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+
+@Dao
+interface PasoDao {
+
+    @Insert
+    suspend fun insert(paso: Paso)
+
+    @Delete
+    suspend fun delete(paso: Paso)
+
+    @Query("SELECT * FROM pasos WHERE recetaId = :recetaId")
+    suspend fun getByRecetaId(recetaId: Long): List<Paso>
+}
