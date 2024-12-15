21be335ec0e9a9cc6656f0b5acb7ab9d9e93935f
simongc04 <simonescuela9@gmail.com>
2024-12-15 23:34:29 +0000

    Archivo terminado en view

--- app/src/main/java/com/simon/proyectofinalut4/view/RecetaViewModel.kt
+++ app/src/main/java/com/simon/proyectofinalut4/view/RecetaViewModel.kt
@@ -1,2 +1,43 @@
 package com.simon.proyectofinalut4.view
 
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.MutableLiveData
+import com.simon.proyectofinalut4.data.Receta
+import kotlinx.coroutines.launch
+
+class RecetaViewModel(application: Application) : AndroidViewModel(application) {
+
+    private val repository: RecetaRepository = RecetaRepository(application)
+    private val _recetas = MutableLiveData<List<Receta>>()
+    val recetas: LiveData<List<Receta>> get() = _recetas
+
+    fun loadRecetas() {
+        viewModelScope.launch {
+            _recetas.value = repository.getAllRecetas()
+        }
+    }
+
+    fun addReceta(receta: Receta) {
+        viewModelScope.launch {
+            repository.addReceta(receta)
+            loadRecetas() // recargar la lista
+        }
+    }
+
+    fun updateReceta(receta: Receta) {
+        viewModelScope.launch {
+            repository.updateReceta(receta)
+            loadRecetas()
+        }
+    }
+
+    fun deleteReceta(receta: Receta) {
+        viewModelScope.launch {
+            repository.deleteReceta(receta)
+            loadRecetas()
+        }
+    }
+}
