d30844a48dd9b8548f56c95eaa34fb646a29488f
simongc04 <simonescuela9@gmail.com>
2024-12-15 23:25:17 +0000

    Codigo creado

--- app/src/main/java/com/simon/proyectofinalut4/data/Receta.kt
+++ app/src/main/java/com/simon/proyectofinalut4/data/Receta.kt
@@ -5,9 +5,7 @@ import androidx.room.PrimaryKey
 
 @Entity(tableName = "recetas")
 data class Receta(
-    @PrimaryKey(autoGenerate = true) val id: Int = 0,
+    @PrimaryKey(autoGenerate = true) val id: Long = 0,
     val nombre: String,
-    val descripcion: String,
-    val tiempoPreparacion: String,
-    val categoriaId: Int
+    val descripcion: String
 )
