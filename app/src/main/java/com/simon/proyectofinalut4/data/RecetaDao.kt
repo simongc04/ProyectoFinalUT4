35321165fc828cb4234111a55215f18279498065
simongc04 <simonescuela9@gmail.com>
2024-12-15 23:29:11 +0000

    Archivo terminado

--- app/src/main/java/com/simon/proyectofinalut4/data/RecetaDao.kt
+++ app/src/main/java/com/simon/proyectofinalut4/data/RecetaDao.kt
@@ -1,2 +1,25 @@
 package com.simon.proyectofinalut4.data
 
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+
+import androidx.room.*
+
+@Dao
+interface RecetaDao {
+
+    @Insert
+    suspend fun insertReceta(receta: Receta)
+
+    @Update
+    suspend fun updateReceta(receta: Receta)
+
+    @Delete
+    suspend fun deleteReceta(receta: Receta)
+
+    @Query("SELECT * FROM recetas")
+    suspend fun getAllRecetas(): List<Receta>
+}
