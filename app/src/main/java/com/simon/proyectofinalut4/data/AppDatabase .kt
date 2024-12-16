002c5151ef0d51ab91ad483e30c87edba5740d8e
simon <simonescuela9@gmail.com>
2024-12-16 13:19:46 +0000

    actualizado

--- app/src/main/java/com/simon/proyectofinalut4/data/AppDatabase .kt
+++ app/src/main/java/com/simon/proyectofinalut4/data/AppDatabase .kt
@@ -5,24 +5,28 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 
-@Database(entities = [Receta::class], version = 1)
+
+@Database(entities = [Receta::class, Paso::class], version = 4)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun recetaDao(): RecetaDao
+    abstract fun pasoDao(): PasoDao  // Correctly return PasoDao here
 
     companion object {
+        @Volatile
         private var INSTANCE: AppDatabase? = null
 
         fun getDatabase(context: Context): AppDatabase {
-            if (INSTANCE == null) {
-                synchronized(AppDatabase::class.java) {
-                    INSTANCE = Room.databaseBuilder(
-                        context.applicationContext,
-                        AppDatabase::class.java,
-                        "receta_database"
-                    ).build()
-                }
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    AppDatabase::class.java,
+                    "receta_database"
+                )
+                    .fallbackToDestructiveMigration()
+                    .build()
+                INSTANCE = instance
+                instance
             }
-            return INSTANCE!!
         }
     }
 }
